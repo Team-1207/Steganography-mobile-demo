bbcdd739f4c4ad8f3dca98fe8a12f39c75f0db5c
Plotnikov Andrey <shemplo@outlook.com>
2023-08-25 18:58:32 +0300

    Remove debug prints to console

--- app/src/main/java/ru/shemplo/steganography/SteganographyEngine.java
+++ app/src/main/java/ru/shemplo/steganography/SteganographyEngine.java
@@ -38,9 +38,7 @@ public class SteganographyEngine {
         for (int i = 0; i < SIZE_PIXELS; i++) {
             for (int j = 0; j < 4; j++) {
                 int bit = (bytes.length >>> ((SIZE_PIXELS - i) * 4 - j - 1)) & 0b1;
-                Log.i ("SE", Integer.toBinaryString (bytes.length) + " " + j + ": " + bit);
                 setPixelBit (bitmap, i, bitsOrder [j], bit);
-                Log.i ("SE", Integer.toBinaryString (getPixel (bitmap, i)));
             }
         }
 
@@ -48,7 +46,6 @@ public class SteganographyEngine {
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 4; k++) {
                     int bit = (bytes [i] >>> (7 - j * 4 - k)) & 1;
-                    Log.i ("SE", Integer.toBinaryString (bytes [i]) + " " + j + "/" + k + ": " + bit);
                     setPixelBit (bitmap, i * 2 + j + SIZE_PIXELS, bitsOrder [k], bit);
                 }
             }
@@ -65,35 +62,19 @@ public class SteganographyEngine {
 
         int maxData = (1 << (SIZE_PIXELS * 4 + 1)) - 1;
         int length = Math.min (takeNumber (sizePixels), maxData);
-        //sb.append (length).append ("|");
 
         byte [] bytes = new byte [length];
         for (int i = 0; i < length; i++) {
             int a = getPixel (bitmap, i * 2 + SIZE_PIXELS), b = getPixel (bitmap, i * 2 + 1 + SIZE_PIXELS);
-
-            /*
-            StringBuilder tsba = new StringBuilder ();
-            StringBuilder tsbb = new StringBuilder ();
-            for (int d = 3; d >= 0; d--) {
-                int ta = (a >>> (d * 8)) & 0xFF, tb = (b >>> (d * 8)) & 0xFF;
-                tsba.append (String.format ("0x%2h (%3d)  ", ta, ta)).append (" ");
-                tsbb.append (String.format ("0x%2h (%3d)  ", tb, tb)).append (" ");
-            }
-            */
-
             bytes [i] = (byte) takeNumber (a, b);
-            //Log.i ("SE", "с: " + String.format ("%8s (%d)", Integer.toBinaryString (character & 0xFF), character));
-
-            //sb.append (Character.toChars (character & 0xFF) [0]);
-            //Log.i ("SE", "r: " + sb);
         }
+
         sb.append (new String (bytes));
 
         return sb.toString ();
     }
 
     private static void setPixel (Bitmap bitmap, int index, int pixel) {
-        Log.i ("SE", "Index: " + index + ", width: " + bitmap.getWidth () + ", height: " + bitmap.getHeight ());
         bitmap.setPixel (index % bitmap.getWidth (), index / bitmap.getWidth (), pixel);
     }
 
@@ -118,9 +99,7 @@ public class SteganographyEngine {
         for (int i = 0; i < rgbas.length; i++) {
             for (int j = 0; j < bitsOrder.length; j++) {
                 int shift = (rgbas.length - i) * 4 - j - 1;
-                Log.i ("SE", "Bit: " + bitsOrder [j] + ", shift: " + shift);
                 int bit = takeAndShift (rgbas [i], bitsOrder [j], shift);
-                Log.i ("SE", "From: " + Integer.toBinaryString (rgbas [i]) + ", bit: " + Integer.toBinaryString (bit));
                 result |= bit;
             }
         }
